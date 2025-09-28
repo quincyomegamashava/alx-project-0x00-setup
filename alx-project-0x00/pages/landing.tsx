97fe8df369e9692f29a6c6dd31bd3326ce2c8365
QuincyMashava <mashavaquincy@gmail.com>
2025-09-28 14:13:48 +0200

    task completion

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,14 @@
+import Card from "@/components/Card";
+
 const Landing: React.FC =  () => {
   return (
-    <div>
+    <div className="p-6 space-y-6">
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   )
 }
